93f4cd9b2eca7cb665fb10ec48b704fe9f0458fd
Levi Smith <96553609+Legiticus@users.noreply.github.com>
2026-04-20 22:01:24 -0700

    Initial Commit

---
+++ processor/main.py
@@ -0,0 +1,12 @@
+"""!
+@file	main.py
+@author Levi Smith
+@brief	The main file for the 3D surface scanning application
+"""
+
+import numpy as np
+import serial
+
+
+if __name__ == "__main__":
+	print('Hello World')
